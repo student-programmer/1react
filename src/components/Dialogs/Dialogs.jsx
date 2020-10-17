548ce1a148b8642daadae0967193a64f57d4df79
Roman Podobny <tazer16179@gmail.com>
2020-10-17 16:04:13 +0300

    7: commit 25 lesson and map arry

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -15,32 +15,30 @@ const DialogItem = (props) => {
 const Message = (props) => {
   return <div className={d.massage}>{props.message}</div>;
 };
-let dialogsData = [
+let dialogs = [
   { id: 1, name: 'Roma' },
   { id: 2, name: 'Nikita' },
   { id: 3, name: 'Vlad' },
   { id: 4, name: 'Ignat' },
 ];
-let messagesData = [
+
+let messages = [
   { id: 1, message: 'Hi' },
   { id: 2, message: 'How much did you earn?' },
   { id: 3, message: 'Five million dolars!' },
 ];
+
+let dialogsElements = dialogs.map((d) => (
+  <DialogItem name={d.name} id={d.id} />
+));
+let messagesElements = messages.map((m) => <Message message={m.message} />);
+
 const Dialogs = (props) => {
   return (
     <div className="app-wrapper-content">
       <div className={d.dialogs}>
-        <div className={d.dialogsItems}>
-          <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
-          <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
-          <DialogItem name={dialogsData[2].name} id={dialogsData[2].id} />
-          <DialogItem name={dialogsData[3].name} id={dialogsData[3].id} />
-        </div>
-        <div className={d.messages}>
-          <Message message={messagesData[0].message}></Message>
-          <Message message={messagesData[1].message}></Message>
-          <Message message={messagesData[2].message}></Message>
-        </div>
+        <div className={d.dialogsItems}>{dialogsElements}</div>
+        <div className={d.messages}>{messagesElements}</div>
       </div>
     </div>
   );
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,17 +2,19 @@ import React, { Component } from 'react';
 import c from './MyPosts.module.css';
 import Post from './Post/Post';
 
-let postData = [
-  { id: 1, message: 'Hi this my first phost', likesCount: 11,  dislike: 12 },
-  { id: 2, message: 'Ok', likesCount: 1,  dislike: 3},
-  { id: 3, message: 'Five million dolars!' },
+let posts = [
+  { id: 1, message: 'Hi this my first phost', likesCount: 11, dislike: 12 },
+  { id: 2, message: 'Ok', likesCount: 1, dislike: 3 },
+  { id: 3, message: 'Five million dolars!', likesCount: 2, dislike: 3 },
 ];
+let postsElements = posts.map((p) => (
+  <Post massage={p.message} like={p.likesCount} dislike={p.dislike} />
+));
 let MyPosts = () => {
   return (
     <div className={c.content}>
       My posts
-      <Post massage={postData[0].message} like={postData[0].likesCount} dislike={postData[0].dislike} />
-      <Post massage={postData[1].message} like={postData[1].likesCount} dislike={postData[1].dislike} />
+      {postsElements}
     </div>
   );
 };
